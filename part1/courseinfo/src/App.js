fba4d21b065e63e4dd4c0412623777522b9bacde
Eugeniu Stina <xplore93@gmail.com>
2021-07-20 17:05:05 +0200

    feature: ex 1.1 done

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+const Content = (props) => {
+  return (
+    <>
+      {Object.entries(props.elements).map((value) => (
+        <p>
+          {value[0]} {value[1]}
+        </p>
+      ))}
+    </>
+  )
+}
+
+const Total = (props) => {
+  return <p>Number of exercises {props.total}</p>
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        elements={{
+          [part1]: exercises1,
+          [part2]: exercises2,
+          [part3]: exercises3
+        }}
+      />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
