6bff53deb09319e084bf26d7d0f21008da7f04d6
Eugeniu Stina <xplore93@gmail.com>
2021-07-20 17:42:33 +0200

    feature: course information step4 done

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -15,7 +15,7 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <>
-      {props.elements.map((value, key) => {
+      {props.parts.map((value, key) => {
         return <Part key={key} part={value.name} exercises={value.exercises} />
       })}
     </>
@@ -23,29 +23,36 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
-  return <p>Number of exercises {props.total}</p>
+  let total = 0
+  props.parts.forEach((element) => {
+    total += element.exercises
+  })
+
+  return <p>Number of exercises {total}</p>
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content elements={[part1, part2, part3]} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
